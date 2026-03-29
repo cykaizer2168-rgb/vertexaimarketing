86f77225c8425cc4629c382c863fe60e779bfca5
Angelo Franco <lukash0915@Angelos-MacBook-Air.local>
2026-03-29 10:03:45 +0800

    Fix: Updated UI and resolved local development issues

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/vertex-light.html',
+        destination: '/vertex-light-final.html',
       },
     ];
   },
